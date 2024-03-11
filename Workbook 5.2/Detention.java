ad53ac487cd45469ea3fa6b63bb1937885bfa3d1
faniyi-akinbobola <110318688+faniyi-akinbobola@users.noreply.github.com>
2024-03-11 01:43:59 +0100

    Add files via upload

---
+++ Workbook 5.2/Detention.java
@@ -0,0 +1,10 @@
+public class Detention {
+    public static void main(String[] args) {
+
+        System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
+        // See Learn the Part for the instructions.
+        for(int i = 1; i < 100; i++){
+            System.out.println(i + ". I will not forget the semi-colons.");
+        }
+    }
+}
